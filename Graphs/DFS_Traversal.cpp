790dba94cd4a9f3960ed4f5e84eb5c77a5429754
Divya Tembhurne <divyatembhurne1107@gmail.com>
2021-10-04 21:59:40 +0530

    Create DFS_Traversal.cpp

---
+++ Graphs/DFS_Traversal.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int>dfs_traversal;
+void dfs(vector<int>adj[], bool vis[], int s)
+{
+	if(vis[s]) return;
+	dfs_traversal.push_back(s);
+	vis[s]=true;
+	for(auto it: adj[s])
+	{
+		dfs(adj,vis,it);
+	}
+}
+int main()
+{
+	int V,E,u,v;
+	cin>>V>>E;
+	vector<int>adj[V+1];
+	for(int i=0;i<E;i++)
+	{
+		cin>>u>>v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	int s;
+	cin>>s;
+	bool vis[V+1];
+	memset(vis,false,sizeof(vis));
+	dfs_traversal.clear();
+	dfs(adj,vis,s);
+	for(int i=0;i<v;i++)
+	{
+		cout<<dfs_traversal[i]<<" ";
+	}
+}
